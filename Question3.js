99b657f325dcbd31e5f61b9f66d7051c855e1e88
sanjitpal123 <146546817+sanjitpal123@users.noreply.github.com>
2024-02-04 20:14:27 +0530

    Update Question3.js

--- Question3.js
+++ Question3.js
@@ -1,54 +1,51 @@
+function makeEgg() {
+  const random = Math.random();
+  const delay = 1000 + random * 4000;
+  return new Promise((res, rej) => {
+    setTimeout(() => {
+      res("Egg is Made!");
+    }, delay);
+  });
+}
+
+function makeToast() {
+  const random = Math.random();
+  const delay = 1000 + random * 4000;
+  return new Promise((res, rej) => {
+    setTimeout(() => {
+      res("Toast is Made!");
+    }, delay);
+  });
+}
+
+function makeTea() {
+  const random = Math.random();
+  const delay = 1000 + random * 4000;
+  return new Promise((res, rej) => {
+    setTimeout(() => {
+      res("Tea is made !");
+    }, delay);
+  });
+}
+
+
+
 function prepareFullBreakFast() {
-  function makeEgg() {
-    const random = Math.random();
-    const delay = 1000 + random * 4000;
-    return new Promise((res, rej) => {
-      setTimeout(() => {
-        res("Egg is Made!");
-      }, delay);
-    });
-  }
-
-  function makeToast() {
-    const random = Math.random();
-    const delay = 1000 + random * 4000;
-    return new Promise((res, rej) => {
-      setTimeout(() => {
-        res("Toast is Made!");
-      }, delay);
-    });
-  }
-
-  function makeTea() {
-    const random = Math.random();
-    const delay = 1000 + random * 4000;
-    return new Promise((res, rej) => {
-      setTimeout(() => {
-       
-        rej("Tea making!");
-      }, delay);
-    });
-  }
-
-  Promise.all([makeEgg(), makeTea(), makeToast()])
-    .then(() => {
-      console.log("Full breakfast is made");
-    })
-    .catch((error) => {
-      console.log("Error occurred:", error);
-    });
-
-  async function execute() {
-    const eggResult = await makeEgg();
-    const toastResult = await makeToast();
-    const teaResult = await makeTea();
-  }
-
-  execute();
+   async function execute()
+   {
+    try{
+     const result= await Promise.all([makeEgg(),makeTea(),makeToast()]);
+      console.log("Full BreakFast is Ready");
+    }catch(error)
+    {
+      console.log("Error comes between:",error);
+    }
+      
+   }
+   execute();
 }
 
 prepareFullBreakFast();
-
 
 
 
