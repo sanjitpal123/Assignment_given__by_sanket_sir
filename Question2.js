ac1e2e4a183d9a97a4b877f0b7404e190c9d9318
sanjitpal123 <146546817+sanjitpal123@users.noreply.github.com>
2024-02-04 20:02:10 +0530

    Update Question2.js

--- Question2.js
+++ Question2.js
@@ -1,34 +1,33 @@
-function makeTea()
-{
-    return new Promise((res,rej)=>{
-        setTimeout(()=>{
-               res("Tea is Ready!");
-        },2000)
+function makeTea() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve("Tea is Ready!");
+        }, 2000);
     });
 }
 
-
-
-function prepareBreakfast()
-{
-    
-     return new Promise((res,rej)=>{
-        setTimeout(()=>{
-          res("Toast is ready!");
-        },3000);
-     });
-
+function prepareBreakfast() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve("Toast is ready !");
+        }, 3000);
+    });
 }
 
-const pr1=makeTea();
-pr1.then((result)=>{
-    console.log(result);
-    return prepareBreakfast();
-}).catch((error)=>{
-    throw error;
-})
-.then((result)=>{
-    console.log(result);
-}).catch((error)=>{
-    console.error(error.mesaage);
-});
+const pr1 = makeTea();
+
+pr1
+    .then((result) => {
+        console.log(result);
+        return prepareBreakfast();
+    })
+    .catch((error) => {
+        throw error; 
+    })
+    .then((result) => {
+        console.log(result);
+    })
+    .catch((error) => {
+        console.error(error.message);
+    });
+
